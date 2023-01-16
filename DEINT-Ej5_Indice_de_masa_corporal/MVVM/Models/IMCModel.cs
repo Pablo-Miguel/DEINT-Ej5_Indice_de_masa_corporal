3bf8d90f7df89af4308648384fb3c82b7d12ca29
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-01-16 13:04:15 +0100

    V1

--- DEINT-Ej5_Indice_de_masa_corporal/MVVM/Models/IMCModel.cs
+++ DEINT-Ej5_Indice_de_masa_corporal/MVVM/Models/IMCModel.cs
@@ -10,13 +10,25 @@ namespace DEINT_Ej5_Indice_de_masa_corporal.MVVM.Models
     [AddINotifyPropertyChangedInterface]
     public class IMCModel
     {
-        public int Altura { get; set; }
-        public int Peso { get; set;}
-        public double ImcResultado { get; set; } = 0;
+
+        private int altura = 25;
+        private int peso = 50;
+
+        public int Altura { get => altura; set {
+                altura = value;
+                calculateIMC(value, Peso);
+            }
+        }
+        public int Peso { get => peso; set {
+                altura = value;
+                calculateIMC(Altura, value);
+            }
+        }
+        public double ImcResultado { get; set; } = 3.5;
         public string ImcDescrp { get; set; } = "Delgadez Severa";
 
-        public void calculateIMC() { 
-            double ImcResultado = Peso / Math.Pow(Peso, 2);
+        public void calculateIMC(int altura, int peso) { 
+            ImcResultado = peso / Math.Pow(altura, 2);
 
             if (ImcResultado < 16) ImcDescrp = "Delgadez Severa";
             else if (16 <= ImcResultado && ImcResultado < 17) ImcDescrp = "Delgadez Moderada";
